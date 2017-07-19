4713802c6f41d77a73ab7b945f8895017437a1ad
Jaap Jansma <jaap@edeveloper.nl>
2017-07-19 11:25:36 +0200

    added a hack to let the local connection behave similar to the remote ones.

--- CMRF/Connection/Local.php
+++ CMRF/Connection/Local.php
@@ -37,8 +37,23 @@ class Local extends Connection {
     } catch (\Exception $e) {
       $call->setStatus(Call::STATUS_FAILED, $e->getMessage());
     }
+
+    // Hack from CiviCRM core to make the reply behave similar as the remote API.
+    // Meaning that a scalar value (a number, string etc.) should be wrapped in an array by the key result.
+    if (is_scalar($reply)) {
+      if (!$reply) {
+        $reply = 0;
+      }
+      $reply = array(
+        'is_error' => 0,
+        'result' => $reply
+      );
+    }
+
     $call->setReply($reply);
     return $reply;
   }
+
 }
+
 
